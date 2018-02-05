fab3042767c9226743601ed748cc6a16f13746da
brekol <brekol@gmail.com>
2018-02-05 18:40:43 +0100

    fix

--- piotr-brek-webapp/src/app/jb/jb-footer.ts
+++ piotr-brek-webapp/src/app/jb/jb-footer.ts
@@ -4,13 +4,20 @@ import { Component } from '@angular/core';
     selector: 'jb-footer',
     template: `
         <div class="jb-footer-separation"></div>
-        <div class="jb-footer-text">
-            <div class="jb-footer-element jb-accent">Drogi projektowanie i nadzór mgr inż. Piotr Bręk</div>
-            <hr>
-            <div class="jb-footer-element">ul. Parkowa 7/3</div>
-            <div class="jb-footer-element">78-600 Wałcz</div>
-            <div class="jb-footer-element">tel: +48 661 550 777</div>
-            <div class="jb-footer-element">email: piotrbrek61@gmail.com</div>
+        <div class="jb-footer-wrapper">
+            <div class="jb-footer-text">
+                <div class="jb-footer-element jb-accent">Drogi projektowanie i nadzór mgr inż. Piotr Bręk</div>
+                <hr>
+                <div class="jb-footer-element">ul. Parkowa 7/3</div>
+                <div class="jb-footer-element">78-600 Wałcz</div>
+                <div class="jb-footer-element">tel: +48 661 550 777</div>
+                <div class="jb-footer-element">email: piotrbrek61@gmail.com</div>
+            </div>
+            <div class="jb-footer-map">
+                <iframe width="600" height="300" frameborder="0" style="border:0"
+                        src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJs1PB5ZNdAUcRycXZxwPaniM&key=AIzaSyBio52dNhb0aU9SWY_rESJm9dedw8Vklpc"
+                        allowfullscreen></iframe>
+            </div>
         </div>
     `,
     styleUrls: ['../../scss/jb-footer.scss']
