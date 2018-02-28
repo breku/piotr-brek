c0c61277dfffa2740f3fe2ffe9bb2444369d84a6
brekol <brekol@gmail.com>
2018-02-28 18:23:51 +0100

    fix

--- piotr-brek-webapp/src/app/jb/jb-footer.ts
+++ piotr-brek-webapp/src/app/jb/jb-footer.ts
@@ -11,6 +11,7 @@ import { Component } from '@angular/core';
                 <div class="jb-footer-element">ul. Parkowa 7/3</div>
                 <div class="jb-footer-element">78-600 Wałcz</div>
                 <div class="jb-footer-element">tel: +48 661 550 777</div>
+                <div class="jb-footer-element">NIP: 765-000-00-20</div>
                 <div class="jb-footer-element">email: piotrbrek61@gmail.com</div>
             </div>
         </div>
